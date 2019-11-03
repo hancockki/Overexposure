610efb6aa8d1655311672938d0b946f02a32fb0b
hancockki <52976054+hancockki@users.noreply.github.com>
2019-11-03 16:44:16 -0500

    Add files via upload
    improved code with better docstrings

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -14,100 +14,32 @@ import csv
 
 global rejectingNodeDict
 global clusterDict
-
-
-# In[32]:
-
-
 rejectingNodeDict = {}
 clusterDict = {}
 
-fh=open("CollegeMsg.txt", encoding='utf=8') # use utf-8 encoding
+# In[32]:
 
-G=fh.readlines()
-#print(G)
-G_College_Msg = nx.Graph()
-for i in G: #iterate through (i,j) pairs, adding edges to graph
-    i = i.split()
-    #print(i, i[0])
-    G_College_Msg.add_node(int(i[0]))
-    G_College_Msg.add_node(int(i[1]))
-    G_College_Msg.add_edge(int(i[0]), int(i[1]))
-    
-n = G_College_Msg.number_of_nodes()
-
-G_DP = nx.Graph()
-
-G_DP.add_edge(8,16,weight=8)
-G_DP.add_edge(7,15,weight=10)
-G_DP.add_edge(7,14,weight=2)
-G_DP.add_edge(6,13,weight=5)
-G_DP.add_edge(6,12, weight=4)
-G_DP.add_edge(5,11, weight=3)
-G_DP.add_edge(4,10, weight=7)
-G_DP.add_edge(3,9, weight=4)
-G_DP.add_edge(3,8, weight=7)
-G_DP.add_edge(2,7, weight=5)
-G_DP.add_edge(2,6, weight=8)
-G_DP.add_edge(1,5, weight=3)
-G_DP.add_edge(1,4, weight=4)
-G_DP.add_edge(0,3, weight=6)
-G_DP.add_edge(0,2, weight=9)
-G_DP.add_edge(0,1, weight=9)
-G_DP.node[0]['weight'] = 8
-G_DP.node[1]['weight'] = 10
-G_DP.node[2]['weight']=12
-G_DP.node[3]['weight']=5
-G_DP.node[4]['weight']=8
-G_DP.node[5]['weight']=4
-G_DP.node[6]['weight']=12
-G_DP.node[7]['weight']=12
-G_DP.node[8]['weight']=8
-G_DP.node[9]['weight']=7
-G_DP.node[10]['weight']=10
-G_DP.node[11]['weight']=5
-G_DP.node[12]['weight']=8
-G_DP.node[13]['weight']=6
-G_DP.node[14]['weight']=6
-G_DP.node[15]['weight']=7
-G_DP.node[16]['weight']=9
-
-G_DP2 = nx.Graph()
-
-G_DP2.add_edge(0,2,weight=7)
-G_DP2.add_edge(0,1,weight=4)
-G_DP2.add_edge(1,4,weight=5)
-G_DP2.add_edge(1,3,weight=9)
-G_DP2.add_edge(0,5,weight=7)
-
-G_DP2.node[0]['weight'] = 15
-G_DP2.node[1]['weight']=3
-G_DP2.node[2]['weight']=7
-G_DP2.node[3]['weight']=3
-G_DP2.node[4]['weight']=14
-G_DP2.node[5]['weight']=2
-
-G_cluster = nx.Graph()
-def setClusterGraphAttributes(G, n):
+#Set the attributes for the cluster graph which we generate randomly
+#input -- n, the number of nodes
+#return -- G, a random tree with n nodes
+def createClusterGraph(n):
+    G = nx.random_tree(200)
     for i in G.nodes():
         rand = random.randint(1,15)
-        rand *= 10000
         G.node[i]['weight'] = rand
         for neighbor in nx.neighbors(G, i):
             rand2 = random.randint(0,10)*10000
             G.add_edge(i, neighbor, weight=rand2)
+    return G
 
+#Set node attributes for the original graph, where each node is an individual and edges represent 
+#connection in the network
 def setAllNodeAttributes(G):
     nodeList = G.nodes()
     for nodeID in nodeList:
         G.node[nodeID]["visited"] = False
         G.node[nodeID]['criticality'] = random.uniform(0, 1)
         G.node[nodeID]["cluster"] = -1
-
-def clearVisitedNodesAndDictionaries(G):
-    setVisitedFalse(G)
-    rejectingNodeDict.clear()
-    clusterDict.clear()
 
 def setVisitedFalse(G):    
     nodeList = G.nodes()
@@ -180,7 +112,6 @@ def labelClusters(G, source, clusterNumber, appeal, thirdAlgorithm=False):
 
 # In[43]:
 
-
 def buildClusteredSet(G, threshold, thirdAlgorithm=False):
 
     # From each node in the nodeList, try to label its cluster. This will return 0 for many nodes, as they are labeled
@@ -191,7 +122,7 @@ def buildClusteredSet(G, threshold, thirdAlgorithm=False):
     nodeList = G.nodes()
     seedSet = []
     clusterCount = 0
-    
+    G_cluster = nx.Graph()
     #Build the clusters
     for nodeID in nodeList:
         if (G.node[nodeID]['criticality'] < threshold) and (G.node[nodeID]['cluster'] == -1):
@@ -202,8 +133,8 @@ def buildClusteredSet(G, threshold, thirdAlgorithm=False):
             make_Cluster_node(G_cluster, clusterCount, summedNeighbors[0])
             clusterCount += 1
     #Choose up-to-k
-    make_cluster_edge(G_cluster, G_test_DP, rejectingNodeDict)    
-    return seedSet 
+    make_cluster_edge(G_cluster, G, rejectingNodeDict)    
+    return G_cluster
 
 
 def makeMatrix(G, n):
@@ -380,9 +311,15 @@ def computePayoff(G, i, k):
     for j in range(0,k):
         if storePayoff[j][i-1] > maxVal:
             maxVal = storePayoff[j][i-1]
+    #print("optimal seed set is:", storeSeeds[j][i-1])
     return (maxVal, storeSeeds[j][i-1])
 
-def DP_Improved(G, i, k):
+#here, we improve dynamic programming by testing different numbers of nodes in different subtrees.
+#we perform bfs from the source to identify subtrees from the root, and then take the n subtrees and disperse the k nodes among
+#them in every possible combination
+#input = G, graph in clustered format
+#       k, number of seeds
+def DP_Improved(G, k):
     #storePayoff = [[0] * i for _ in range(k)] #store payoff
     #storeSeeds = [[[]] * i for _ in range(k)] #store seeds at each stage
     tree = nx.bfs_tree(G, 1)
@@ -390,7 +327,6 @@ def DP_Improved(G, i, k):
     print(nodes)
     print("root is", nodes[0])
     neighbors = nx.neighbors(G, nodes[0])
-    print(neighbors)
     subgraph_list = [] #store subgraph
     for node in neighbors: #we want to compute payoff for the subtree 
         #print(node)
@@ -398,24 +334,28 @@ def DP_Improved(G, i, k):
         #print("subgraph:", subgraph)
         subgraph_list.append(subgraph)
     num_children = len(subgraph_list)
-    #print("num subgraphs:", num_children)
+    print("num subgraphs:", num_children)
     part = partitions(k, num_children)
     amount = 0
-    storePayoffs = []
+    nodes_picked = []
+    storePayoffs = {}
     for p in part: #get each partition of the seeds
         #print(p)
         for i, subgraph in zip(range(0,num_children), subgraph_list): #pick the correct number of seeds for each subgraph
-            G_sub = G.subgraph(subgraph)
-            j = G_sub.number_of_nodes()
+            G_sub = G.subgraph(subgraph) #we make it a subgraph of G
+            j = G_sub.number_of_nodes() 
             #print("nodes in subgraph:", j)
             if p[i] != 0:
-                amountCur = computePayoff(G_sub, j, p[i])
-                amount += amountCur[0]
+                #print("num seeds is:", p[i])
+                amountCur = computePayoff(G_sub, j, p[i]) #get payoff for j seeds in the subgraph
+                amount += amountCur[0] #add payoff 
+                nodes_picked.append(amountCur[1])
        # print('payoff is:', amountCur)
-        storePayoffs.append(amount)
+        storePayoffs[amount] = nodes_picked
         amount = 0
     maxval = max(storePayoffs) #get largest subset value
     print("max val is:", maxval)
+    #print(storePayoffs)
     return maxval
 
 
@@ -438,10 +378,11 @@ def bfs(G, node, source):
             if neighbor in subgraph:
                 #print("already picked", neighbor)
                 continue
-            elif neighbor != source:
+            elif neighbor != source and neighbor != node:
                 queue.append(neighbor)
                 subgraph.append(neighbor)
                 #print("adding:", neighbor)
+    print("subgraph is", subgraph)
     return subgraph
 
 #we defined a new cluster and are adding a node to the cluster graph, whose weight is the number of accepting nodes in that cluster
@@ -449,6 +390,9 @@ def make_Cluster_node(G, clusterNum, weight):
     G.add_node(clusterNum)
     G.node[clusterNum]['weight'] = weight
 
+#takes the rejecting node dictionary, which maps cluster number to rejecting nodes, and assigns a weight for each pair of clusters
+#that share rejecting nodes
+#input -- original graph, cluster graph, rejecting node dictionary
 def make_cluster_edge(G_cluster, G_orig, rejectingNodesDict):
     for clusterNum, rejNodes in rejectingNodesDict.items():
         for clusterNum2, rejNodes2 in rejectingNodesDict.items():
@@ -469,31 +413,67 @@ def make_cluster_edge(G_cluster, G_orig, rejectingNodesDict):
                     G_cluster.add_edge(clusterNum, clusterNum2, weight=weight)
                     print("intersection between nodes ", clusterNum, clusterNum2, "is:", intersection, "of weight", weight)
 
-start_time = time.time()
 
-G_test_DP = nx.random_tree(200)
-numNode = G_test_DP.number_of_nodes()
+#Here, we read in the file from SNAP, and read it line by line. Each line is composed of the edges (u,v) as well as the 
+#time stamp for creating the graph
+#returns a graph of college students connected by edges, with no attributes
+def college_Message():
+    fh=open("CollegeMsg.txt", encoding='utf=8') # use utf-8 encoding
+    G=fh.readlines()
+    G_College_Msg = nx.Graph()
+    for i in G: #iterate through (i,j) pairs, adding edges to graph
+        i = i.split()
+        #print(i, i[0])
+        G_College_Msg.add_node(int(i[0]))
+        G_College_Msg.add_node(int(i[1]))
+        G_College_Msg.add_edge(int(i[0]), int(i[1]))
+    setAllNodeAttributes(G_College_Msg)
+    print("sucess")
+    G = buildClusteredSet(G_College_Msg, 0.7)
+    print("cluster dict:", clusterDict)
+    print("rej node dict", rejectingNodeDict)
+    test1 = tryDP(G, G.number_of_nodes(), 6)
+    maxval = DP_Improved(G, 6)
+    print("payoff test DP is: ", test1)
+    print("payoff subtree DP is:", maxval)
+    clearVisitedNodesAndDictionaries(G_College_Msg)
+    return G
 
-setClusterGraphAttributes(G_test_DP, numNode)
-setAllNodeAttributes(G_test_DP)
-seeds = buildClusteredSet(G_test_DP, 0.7)
-print("success:", seeds)
-print("cluster dict:", clusterDict)
-print("rej node dict", rejectingNodeDict)
-weight1 = nx.get_node_attributes(G_cluster, name='weight')
-weight2 = nx.get_edge_attributes(G_cluster, name='weight')
-print(weight1, weight2)
-makeMatrix(G_test_DP, numNode)
+#method to test whether our input graph is correctly forming clusters, then runs dynamic programming
+#input -- n, number of nodes in random graph
+#           c, criticality
+#           k, number of clusters to seed
+def testOriginaltoCluster(n, c, k):
+    G_test = nx.random_tree(n)
+    nx.draw_networkx(G_test, pos=None, arrows=False, with_labels=True)
+    plt.show()
+    setAllNodeAttributes(G_test)
+    G_cluster = buildClusteredSet(G_test, c)
+    print("cluster dict:", clusterDict)
+    print("rej node dict", rejectingNodeDict)
+    test1 = tryDP(G_cluster, G_cluster.number_of_nodes(), k)
+    maxval = DP_Improved(G_cluster, k)
+    print("payoff test DP is: ", test1)
+    print("payoff subtree DP is:", maxval)
+    clearVisitedNodesAndDictionaries(G_cluster)
+    makeMatrix(G_cluster, G_cluster.number_of_nodes())
+    return G_cluster
 
-print(weight1,weight2)
+#clear dictionaries for next graph to test
+def clearVisitedNodesAndDictionaries(G):
+    setVisitedFalse(G)
+    rejectingNodeDict.clear()
+    clusterDict.clear()
 
-test1 = tryDP(G_cluster, G_cluster.number_of_nodes(), 6)
-end_time = time.time()
-maxval = DP_Improved(G_cluster, numNode, 6)
-print("payoff test DP is: ", test1, end_time - start_time)
-print("payoff subtree DP is:", maxval)
 
-nx.draw_networkx(G_cluster, pos=None, arrows=False, with_labels=True)
-plt.show()
-nx.draw_networkx(G_test_DP, pos=None, arrows=False, with_labels=True)
-plt.show()
+
+#main function, used for calling things
+def main():
+    G = testOriginaltoCluster(100, 0.8, 3)
+    #testRandomCluster()
+    #G = college_Message()
+
+    nx.draw_networkx(G, pos=None, arrows=False, with_labels=True)
+    plt.show()
+
+main()
