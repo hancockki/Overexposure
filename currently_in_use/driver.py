d71c503ba3535bfe69a7cc507e4775587fc6be41
hancockki <hancock6121@gmail.com>
2021-03-04 14:32:59 -0500

    better organization

--- currently_in_use/driver.py
+++ currently_in_use/driver.py
@@ -4,6 +4,7 @@ import greedy_approx_algorithms as greedy
 import networkx as nx
 import matplotlib.pyplot as plt
 import sys
+import linear_program as lp
 
 def runTests():
     #create cluster greaph
@@ -11,7 +12,7 @@ def runTests():
     G = cc.createClusterGraph(20, 10)
     #compute payoff for greedy DP
     max_val_greedyDP = dp.greedyDP(G, G.number_of_nodes(), 3)
-    sys.stdout = open("currently_in_use/results_details.txt", "w")
+    sys.stdout = open("currently_in_use/results_details.txt", "a")
     #print info about the graph
     print_info(G)
     print("\nGreedy DP Payoff: ", max_val_greedyDP)
@@ -23,11 +24,16 @@ def runTests():
     #compute payoff for recursive DP
     payoff_root, payoff_no_root = dp.runRecursiveDP(G, 3)
     print("Recursive DP payoff: \n Root: ", payoff_root, "\n No Root: ", payoff_no_root)
+
+    lp.lp_setup(G)
     sys.stdout.close()
     with open('currently_in_use/compare_results.txt', 'a') as results:
         results.write('\n'+ str(max_val_greedyDP[0]) + '\t\t\t' + str(payoff) + '\t\t\t' + str(payoff_root) + ' ' + str(payoff_no_root))
     results.close()
-    #print graph
+    printGraph(G)
+
+""" display graph """
+def printGraph(G):
     plt.figure(2)
     pos = nx.spring_layout(G)
     nx.draw(G, pos)
@@ -41,9 +47,14 @@ def runTests():
     plt.show()
 
 def print_info(G):
+    print('\nNext Test:\n')
     data = G.edges.data()
+    weights = G.nodes.data('weight')
+    print('Node weights: ')
+    for node in weights:
+        print("Node:", node[0], "Weight:", node[1])
     for item in data:
-       print("Edge between", item[0], item[1], " Weight: ", item[2]['weight'])
+        print("Edge between", item[0], item[1], " Weight: ", item[2]['weight'])
 
 #main function, used for calling things
 def main():
--- currently_in_use/linear_program.py
+++ currently_in_use/linear_program.py
@@ -1,29 +1,22 @@
 import pulp
-import dynamic_programming_cleaned as dp
-
-def get_input_graph():
-    """
-    Make a cluster graph to run the lp on
-    """
-    G_cluster, G_cluster2, tree_decomp = dp.testOriginaltoCluster(12,0.5)
-    dp.print_info(G_cluster2)
-    return G_cluster2
+import create_clusters as cc
 
 def lp_setup(G):
     """
     Set up our LP based on the cluster data. We want each cluster and rejecting node to be a variable, 
     with the constraint that if you pick a cluster you must pick the rejecting nodes it is connected to.
     """
+    NODES = EDGES = range(G.number_of_nodes())
     maximize_payoff = pulp.LpProblem("Overexposure_Maximization", pulp.LpMaximize)
+    choices = pulp.LpVariable.dicts("Choice", (NODES), cat="Integer")
     cluster_variables = []
     edge_variables = []
     # loop through edge data to make our constraints for the lp
     for edge_data in G.edges.data():
-        rej_node = edge_data[2]['data'][0]
+        rej_node = edge_data[2]['weight']
         if edge_data[0] not in cluster_variables:
             continue
 
-get_input_graph()
 # define our LP problem
 
 """
